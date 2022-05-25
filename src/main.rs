8000d3a93e9cc298b815959240a63ee53071d0ae
Daiki48 <daiki48.engineer@gmail.com>
2022-05-25 21:27:30 +0900

    clap add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
